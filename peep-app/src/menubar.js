b067947d3490aea1f7fd15bd96bb79789e527d16
Kanny Ho <hmx@hmx.local>
2020-10-15 17:37:25 -0400

    updated menubar,js

--- peep-app/src/menubar.js
+++ peep-app/src/menubar.js
@@ -2,10 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Options extends React.Component {
+class OptionBar extends React.Component {
     render() {
         return (
-            <button className = "optbutton" onClick = {this.props.OnClick}>
+            <button className="optbutton" onClick={()=>this.props.onClick()}>
                 {this.props.value}
             </button>
         )
@@ -15,30 +15,26 @@ export default class Menubar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            options: Array(4).fill(null)
-            // Homepage(Into), About Us, Simple Diagram/Sa
+            options : [
+                "Home",
+                "About Us",
+                "Our Values",
+                "Sign In/Sign Up"
+            ]
         };
     }
-    // renderOptions(i) {
-    //     return(
-    //         <Optionbar 
-    //          value = {this.state.options[i]}
-    //         />
-    //     );
-    // }
+
+    handleClick(i){
+        alert("you clicked "+this.state.options[i]);
+    }
+    
     render() {
-        this.state.options[0] = "Home"
-        this.state.options[1] = "About Us"
-        this.state.options[2] = "Values"
-        this.state.options[3] = "Sign In/Up"
-        
         return (
             <div>
-                <Options  value = {this.state.options[0]} />
-                <Options  value = {this.state.options[1]} />
-                <Options  value = {this.state.options[2]} />
-                <Options  value = {this.state.options[3]} />
-                
+                <OptionBar  value={this.state.options[0]} onClick={() => this.handleClick(0)} />
+                <OptionBar  value={this.state.options[1]} onClick={() => this.handleClick(1)} />
+                <OptionBar  value={this.state.options[2]} onClick={() => this.handleClick(2)} />
+                <OptionBar  value={this.state.options[3]} onClick={() => this.handleClick(3)} />
             </div>
         );
     }
